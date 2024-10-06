4acf3e463ffb64be086a1979e534c934f9648d87
tocodevictor1995 <132182198+tocodevictor1995@users.noreply.github.com>
2024-10-06 03:16:15 +0100

    s

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express');
-const app = express();
+const mongodb = require('./db/connect');
+require('dotenv').config();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/', require ('./routes')); 
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)});
-    
-    
-    
-    
-    
-    
+app.use('/contacts', require('./routes/contacts'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }
+});

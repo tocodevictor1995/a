a502d38ca607f5bf1fd5aa0e5ab9b10ef39ea318
tocodevictor1995 <132182198+tocodevictor1995@users.noreply.github.com>
2024-10-06 01:58:39 +0100

    s

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
 const express = require('express');
-const { initDb } = require('./config/db');
-const contactsRoutes = require('./routes/contacts');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.use('/contacts', contactsRoutes);
-
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-initDb((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  }
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
